58d2c8dc72f2ca74f3002781596ebda01d1accab
Gerard <gerard.gandionco@gmail.com>
2023-04-13 17:57:04 -0700

    Initial commit

---
+++ src/main/java/com/ass/brainbitesprototype/BrainbitesPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.ass.brainbitesprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainbitesPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainbitesPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ass/brainbitesprototype/BrainbitesPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ass.brainbitesprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainbitesPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
