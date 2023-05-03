f85b34f522d92e8bf3e2d620928b4180e5a1b901
Gerard <gerard.gandionco@gmail.com>
2023-05-02 20:13:47 -0700

    Establish navbar, footer, and main content for the landing

---
+++ src/main/java/com/ass/brainbitesprototype/controllers/LandingPageController.java
@@ -0,0 +1,13 @@
+package com.ass.brainbitesprototype.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LandingPageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
