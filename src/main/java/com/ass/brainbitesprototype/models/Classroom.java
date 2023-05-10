6cbd42fc213156e3c4ef63eb614f59203ef044c3
Gerard <gerard.gandionco@gmail.com>
2023-05-09 23:05:59 -0700

    Configure new postgres database with fly.io for a now-running application

--- src/main/java/com/ass/brainbitesprototype/models/Classroom.java
+++ src/main/java/com/ass/brainbitesprototype/models/Classroom.java
@@ -1,7 +1,24 @@
 package com.ass.brainbitesprototype.models;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "classrooms")
 public class Classroom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // TODO what does the classroom have?
+    private String name;  // ??
 }
