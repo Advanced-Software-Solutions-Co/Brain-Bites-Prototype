f3d6ecc109a3c7e9e518adf8df8144c217053ae1
RCald_qdveicr <RCaldera674@gmail.com>
2023-05-09 18:14:49 -0700

    Delete print statements

--- src/main/java/com/ass/brainbitesprototype/BrainbitesPrototypeApplication.java
+++ src/main/java/com/ass/brainbitesprototype/BrainbitesPrototypeApplication.java
@@ -8,8 +8,6 @@ public class BrainbitesPrototypeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrainbitesPrototypeApplication.class, args);
-		System.out.println("Hello Test form RC");
-		System.out.println("Test from Gerard");
 	}
 
 
